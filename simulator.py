e9082967d22dcc541ff6f6190ec1d2ca0560c369
Ismaele <tiados04@gmail.com>
2025-06-20 10:29:17 +0200

    feat: added ray and segment intersection function

--- simulator.py
+++ simulator.py
@@ -1,3 +1,6 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 #defining walls, segments of (x1, y1), (x2, y2)
 walls = [
@@ -12,3 +15,31 @@ walls = [
     ((3, 7), (3, 3)),
 ]
 
+#intersect_ray-segment, rayOrigin: radius of the ray, rayDir: direction of the ray, seg is the segment
+def intersect_ray_casting(rayOrigin,rayDir,seg):
+    p,r=np.array(rayOrigin), np.array(rayDir)
+    a,b=np.array(seg[0]), np.array(seg[1])-np.array(seg[0])
+    #b is the direction vector of the segment
+
+    #2x2 matrix of the linear system
+    M=np.column_stack((-r,b))
+
+    try:
+        solution=np.linalg.solve(M,a-p)
+    except np.linalg.LinAlgError:
+        #singolar matrix -> no solution
+        return None
+    
+    t,u=solution
+
+    #valid intersection
+    if t>=0 and 0<=u<=1:
+        #intersection point
+        intersectionPoint=p+t*r
+        return intersectionPoint
+    
+    return None
+
+
+def lidar_scan(pos,num_beans=120,max_range=15.0):
+    
